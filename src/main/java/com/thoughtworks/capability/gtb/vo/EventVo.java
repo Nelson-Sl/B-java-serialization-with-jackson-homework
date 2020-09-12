4ac8948f627677667e4096ca81f25f300d840b79
Shen Liao <shen.liao@thoughtworks.com>
2020-09-12 23:38:42 +0800

    [Nelson Liao] feat: finish problem 1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,12 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.serializer.timeFormatSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +19,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = timeFormatSerializer.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }

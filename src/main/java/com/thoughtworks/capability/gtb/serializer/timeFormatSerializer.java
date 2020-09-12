7bcd1f2371edeff9253e70b07c6e8933fd3062a3
Shen Liao <shen.liao@thoughtworks.com>
2020-09-12 23:39:51 +0800

    [Nelson Liao] feat: problem 1 add timeformatSerializer

---
+++ src/main/java/com/thoughtworks/capability/gtb/serializer/timeFormatSerializer.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.capability.gtb.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class timeFormatSerializer extends StdSerializer<Date> {
+    protected timeFormatSerializer() {
+        super(Date.class);
+    }
+
+    @Override
+    public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeNumber(date.getTime());
+    }
+}
